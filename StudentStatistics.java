a99181c5a586616c1b8c766f56b7622ca95b75f8
sofwh <sirjanbaniya@gmail.com>
2023-09-19 23:33:07 +1000

    Created Student Class and Initialise variables

---
+++ StudentStatistics.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.Arrays;
+
+class Student {
+    String lastName;
+    String firstName;
+    String studentID;
+    double a1Mark;
+    double a2Mark;
+    double a3Mark;
+    double totalMark;
+
+    public Student(String lastName, String firstName, String studentID, double a1Mark, double a2Mark, double a3Mark) {
+        this.lastName = lastName;
+        this.firstName = firstName;
+        this.studentID = studentID;
+        this.a1Mark = a1Mark;
+        this.a2Mark = a2Mark;
+        this.a3Mark = a3Mark;
+        
+    }
+
+   
+}
+
+public class StudentStatistics {
+    public static void main(String[] args) {
+        
+    }
+
+    
+}
