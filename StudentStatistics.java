a954e7ca4c968cc15e62e71a0849c2c45b7d4c76
sofwh <sirjanbaniya@gmail.com>
2023-09-22 01:29:50 +1000

    Method to read Student Data from text file created

--- StudentStatistics.java
+++ StudentStatistics.java
@@ -17,16 +17,61 @@ class Student {
         this.a1Mark = a1Mark;
         this.a2Mark = a2Mark;
         this.a3Mark = a3Mark;
-        
-    }
-
-   
-}
-
-public class StudentStatistics {
-    public static void main(String[] args) {
-        
+       
     }
 
     
 }
+
+public class StudentStatistics {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the name of the input file: ");
+        String fileName = scanner.nextLine();
+
+        Student[] students = readStudentDataFromFile(fileName);
+
+        if (students != null) {
+            System.out.println("Student data from the file.");
+        } else {
+            System.out.println("Error reading student data from the file.");
+        }
+    }
+
+    private static Student[] readStudentDataFromFile(String fileName) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
+            String line;
+            int lineNumber = 0;
+            Student[] students = new Student[70]; 
+
+            while ((line = reader.readLine()) != null) {
+                lineNumber++;
+                if (lineNumber == 1) {
+                    // Skip the first line (unit name)
+                    continue;
+                }
+
+                String[] parts = line.split(",");
+                if (parts.length >= 5) { // Check for at least 5 columns (A1, A2, A3)
+                    String lastName = parts[0].trim();
+                    String firstName = parts[1].trim();
+                    String studentID = parts[2].trim();
+                    double a1Mark = Double.parseDouble(parts[3]);
+                    double a2Mark = Double.parseDouble(parts[4]);
+                    double a3Mark = 0.0; // Default value if A3 is missing
+
+                    if (parts.length >= 6) { // Check if A3 is present
+                        a3Mark = Double.parseDouble(parts[5]);
+                    }
+
+                    students[lineNumber - 2] = new Student(lastName, firstName, studentID, a1Mark, a2Mark, a3Mark);
+                }
+            }
+            return students;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
+
